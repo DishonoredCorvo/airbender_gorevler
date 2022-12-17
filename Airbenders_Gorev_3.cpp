8fb80a1e9a380f3e50d1d928bb28f031b2fec2ca
DishonoredCorvo <117619523+DishonoredCorvo@users.noreply.github.com>
2022-12-17 12:23:43 +0300

    Add files via upload

---
+++ Airbenders_Gorev_3.cpp
@@ -0,0 +1,32 @@
+
+#include <vector>
+#include <iostream>
+
+template <typename T>
+
+std::vector<std::vector<T>> matris_yap(T x, int satir, int sutun) {
+
+    std::vector<std::vector<T>> matris;
+    std::vector<T> satir_vec;
+
+    for (int i = 0; i < sutun; i++) {
+        satir_vec.push_back(x);
+    }
+    for (int j = 0; j < satir; j++) {
+        matris.push_back(satir_vec);
+    }
+
+    return matris;
+}
+
+
+
+
+int main()
+{
+    
+   
+
+}
+
+
